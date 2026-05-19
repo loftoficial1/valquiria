fc7767142c1666bebff0e9188a44e5a42b58463f
zezocaos <loftoficial1@gmail.com>
2026-05-19 18:50:11 -0300

    Update script.js

--- admin/script.js
+++ admin/script.js
@@ -1,1 +1,46 @@
+const SUPABASE_URL =
+  "SUA_URL";
 
+const SUPABASE_KEY =
+  "SUA_ANON_KEY";
+
+const client =
+  supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
+
+/* LOGIN */
+
+async function login() {
+
+  const email =
+    document.getElementById(
+      "email"
+    ).value;
+
+  const password =
+    document.getElementById(
+      "password"
+    ).value;
+
+  const {
+    error
+  } = await client.auth.signInWithPassword({
+
+    email,
+    password
+
+  });
+
+  if(error) {
+
+    alert(error.message);
+
+    return;
+
+  }
+
+  window.location.href =
+    "dashboard.html";
+}
